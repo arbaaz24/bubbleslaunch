d0b3c575170376caa37b0b1e1bcbb271aefbc550
Arbaaz <nasaarbaaz@gmail.com>
2025-08-30 23:29:10 -0500

    updated the href on download button to point to setup.exe in same rpo

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ function App() {
                 <td>Windows</td>
                 <td>
                   <div className="download-cell">
-                    <a href="downloadbubbles.com" className="download-link" ref={tableDownloadRef} onClick={(e) => { 
+                    <a href="/downloads/setup.exe" className="download-link" ref={tableDownloadRef} onClick={(e) => { 
                       e.preventDefault(); 
                       trackDownload('Windows'); 
                       releaseBubbles(tableDownloadRef); 
